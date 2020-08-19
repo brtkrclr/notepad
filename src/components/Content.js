b3b9b5bd899d8e4ffc4c27811aa9b44479ba9829
Berat <68691845+brtkrclr@users.noreply.github.com>
2020-08-19 16:18:43 +0300

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,46 +3,54 @@ import {Row, Col, Button} from 'antd';
 import TextArea from "antd/es/input/TextArea";
 
 const Content = () => {
-    const [text, setText] = useState([""]);
+    const [text, setText] = useState("");
+    const [list, setList] = useState([]);
+
+
     const onInputChange = (event) => {
         setText(event.target.value)
 
     };
     const onFormSubmit = (event) => {
         event.preventDefault();
-        onFormSubmit(text);
+        console.log(text)
+        setList([...list, {text}])
+        //can be removed if we are going to use a button !
     };
+
     return (
         <div>
             <Row style={{background: "white"}}>
                 <Col className={"left column"}
-                    span={12}>
+                     span={12}>
                     <h3>
                         NOTES
-                      {console.log({text})}
-                      <div onSubmit={onFormSubmit}></div>
                     </h3>
+                    <ul>
+                        {list.map((list) => (
+                            <li>{list.text}</li>
+                        ))}
+                    </ul>
+            </Col>
+            <Col
+                span={12}>
+                <h3>
+                    Enter Note
+                </h3>
+                <TextArea
+                    style={{textAlign: "center"}}
+                    placeholder={"Enter Your Note!"}
+                    onChange={onInputChange}
+                /><hr/>
+                <Button onClick={onFormSubmit}>
+                    Add
+                </Button>
 
-                </Col>
 
-                    <Col
-                        span={12}>
-                        <h3>
-                            Enter Note
-                        </h3>
-                        <TextArea
-                            style={{textAlign: "center"}}
-                            placeholder={"Enter Your Note!"}
-                            onChange={onInputChange}
-                        />
-                        <hr/>
-                        <Button onClick={(event) => {
-                            setText(event.target.value)
-                        }}>Add</Button>
-                    </Col>
-            </Row>
-        </div>
-    );
+            </Col>
+        </Row>
+</div>
+);
 
 }
 
