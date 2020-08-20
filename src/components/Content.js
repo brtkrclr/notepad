502d0257635e564d852c118a8654c6e41e9bfba2
Berat <68691845+brtkrclr@users.noreply.github.com>
2020-08-20 12:20:57 +0300

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,54 +3,94 @@ import {Row, Col, Button} from 'antd';
 import TextArea from "antd/es/input/TextArea";
 
 const Content = () => {
-    const [text, setText] = useState("");
+    const [note, setNote] = useState("");
+    const [title,setTitle]=useState([]);
     const [list, setList] = useState([]);
 
-
-    const onInputChange = (event) => {
-        setText(event.target.value)
-
+    const onNoteChange = (event) => {
+        setNote(event.target.value)
     };
+
+    const onTitleChange = (event) => {
+        setTitle(event.target.value)
+    };
+
     const onFormSubmit = (event) => {
         event.preventDefault();
-        console.log(text)
-        setList([...list, {text}])
-        //can be removed if we are going to use a button !
+        setList([...list,{title}])
+
+        console.log(note)
+         //can be removed if we are going to use a button !
     };
+    const editNotes=(event)=>{
+        event.preventDefault();
+        setTitle([...title,{note}])
+    }
+
 
     return (
         <div>
             <Row style={{background: "white"}}>
-                <Col className={"left column"}
+                <Col className={"leftColumn"}
                      span={12}>
                     <h3>
                         NOTES
                     </h3>
                     <ul>
                         {list.map((list) => (
-                            <li>{list.text}</li>
+                            <li>{list.title}
+                            <button onClick={editNotes} icon={"editIcon"}>  </button>
+
+                            </li>
                         ))}
                     </ul>
-            </Col>
-            <Col
-                span={12}>
-                <h3>
-                    Enter Note
-                </h3>
-                <TextArea
-                    style={{textAlign: "center"}}
-                    placeholder={"Enter Your Note!"}
-                    onChange={onInputChange}
-                /><hr/>
-                <Button onClick={onFormSubmit}>
-                    Add
-                </Button>
+                </Col>
 
 
-            </Col>
-        </Row>
-</div>
-);
+                <Col className={"rightColumn"} span={12}>
+                    <h3>
+                        Enter Note
+                    </h3>
+
+                    <form>
+
+                        <input
+                            style={{textAlign: "center"}}
+                            placeholder={"Title"}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your username!',
+                                },
+                            ]}
+                            onChange={onTitleChange}
+                        />
+                        <hr/>
+                        <TextArea
+                            style={{textAlign: "center"}}
+                            placeholder={"Enter Your Note!"}
+                            onChange={onNoteChange}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your username!',
+                                },
+                            ]}
+                        />
+
+                        <hr/>
+                        <Button onClick={onFormSubmit}>
+                            Add
+                        </Button>
+
+
+                    </form>
+
+
+                </Col>
+            </Row>
+        </div>
+    );
 
 }
 
